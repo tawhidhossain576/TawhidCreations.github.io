87c31de36a91a5f47fc7ef5b63969ca9ee6b3417
tawhidhossain576 <51738990+tawhidhossain576@users.noreply.github.com>
2019-06-12 10:39:17 +0100

    Add files via upload
    good method

---
+++ Nasimvai.js
@@ -0,0 +1,51 @@
+function webdesign_hover(){
+	document.querySelector(".oneshover").classList.add("show");
+	
+}
+function app_hover(){
+	document.querySelector(".twoshover").classList.add("show");
+	
+}
+function Branding_hover(){
+	document.querySelector(".fourshover").classList.add("show");
+	
+}
+function Ecommerce_hover(){
+	document.querySelector(".threeshover").classList.add("show");
+	
+}
+function DigiMark_hover(){
+	document.querySelector(".fiveshover").classList.add("show");
+	
+}
+function Webdesign_out(){
+	document.querySelector(".oneshover").classList.remove("show");
+	
+}
+function app_out(){
+	document.querySelector(".twoshover").classList.remove("show");
+	
+}
+function Ecommerce_out(){
+	document.querySelector(".threeshover").classList.remove("show");
+	
+}
+function branding_out(){
+	document.querySelector(".fourshover").classList.remove("show");
+	
+}
+function DigiMark_out(){
+	document.querySelector(".fiveshover").classList.remove("show");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
